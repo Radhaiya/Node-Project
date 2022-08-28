61df8723521fd69479fe18f969e792061a7c1b61
Radhaiya <kulkarnir27999@gmsil.com>
2022-08-28 16:34:13 +0530

    server changes

--- routes/routes.js
+++ routes/routes.js
@@ -4,22 +4,19 @@ const user = require('../model/userSchema')
 
 route.post('/register', async (req, res) => { //register page route 
 
-    const userExist = await user.findOne({ email: req.body.email })
+    const userExist = await user.findOne({ email: req.body.email })//Checks Whether the email already exist in Monog
 
     if (userExist) {
         res.status(400).send("User Already Exist")
     }
-
-
     else {
-        const newuser = new user({
+        const newuser = new user({   //Gets values from frontend and creates the user Object from userSchema.js
             name: req.body.name,
             email: req.body.email,
             password: req.body.password
-        })
-
-
-        const saveduser = await newuser.save()
+        })  
+        
+        await newuser.save() // Saves the user in database
 
         res.send("Registered Successfully")
     }
@@ -28,23 +25,18 @@ route.post('/register', async (req, res) => { //register page route
 route.post('/login', async (req, res) => {  // login page  route
     const userExist = await user.findOne({ email: req.body.email })
     if (userExist) {
-         
+
         if (userExist.password == req.body.password) {
-           res.send("Login Success")
+            res.send("Login Success")
         }
-        
+
         else {
             res.status(400).send("Invalid Credentials")
         }
-
-
-       
     }
     else {
         res.status(400).send("User doesn't Exist")
     }
-
-
 })
 
 module.exports = route              // exporting route module
